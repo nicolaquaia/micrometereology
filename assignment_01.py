960b03555eef19224fdb0ad3e4e141778ed0a3b1
Nicola Quaia <144346869+nicolaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-09-15 15:56:48 +0200

    Update assignment_01.py

--- assignment_01.py
+++ assignment_01.py
@@ -10,7 +10,7 @@ hov = pd.read_csv('hovsore_1.txt', delimiter=',',header=None)
 velocity = hov[1]
 hov_mean = np.mean(velocity)    #= mu
 hov_std = np.std(velocity)      #= sigma
-print(hov_mean, hov_std)
+#print(hov_mean, hov_std)
 
 # Gaussian distribution
 def gaussian(x, mu, sig):
@@ -24,7 +24,7 @@ plt.plot(x, y_gau, color='b',label='Gaussian dist')
 # histogram
 y_hist = np.histogram(velocity, bins=100, density=True)
 width_bar = (max(velocity) - min(velocity))/100
-print(width_bar)
+#print(width_bar)
 plt.bar(x, y_hist[0], width=0.2, align='center', color='r', label='histogram')
 
 # plot
@@ -33,22 +33,34 @@ plt.xlabel("x")
 plt.ylabel("PDF")
 plt.legend()
 
-plt.show()
+#plt.show()
 
 
 
 
 # sprog
 sprog = pd.read_csv('sprog.tsv', delimiter='\t', header=None)
-speed70 = sprog[1]
-direct65 = sprog[2]
-speed70_mean = np.mean(speed70)    #= mu
-speed70_std = np.std(speed70)      #= sigma
-#print(speed70_mean, speed70_std)
+# columns: time | wind speed 70 | direct 67.5 | direct 70
 
+speed70 = sprog[1]  # 23249 of 99.99
+direct67 = sprog[2]
+direct70 = sprog[3]
 
-counter = 0
-for i in direct65:
-    if i == 999:
-        counter = counter + 1
-#print(counter)
+speed_list = []
+direct_list = []
+
+#for i in range(len(speed70)):
+for i in range(len(speed70)):
+    if speed70[i] < 90:
+        speed_list.append(speed70[i])
+        if direct70[i] < 900:
+            direct_list.append(direct70[i])
+        elif direct67[i] < 900:
+            direct_list.append(direct67[i])
+        else:
+            direct_list.append(0)
+
+speed_list_mean = np.mean(speed_list)       #= mu
+speed_list_std = np.std(speed_list)
+print(speed_list_mean)                      # 8.235566499680502
+print(speed_list_std)                       # 3.907911767517807
