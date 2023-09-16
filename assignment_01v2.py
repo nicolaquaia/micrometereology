a67e8de2d7ea87b8dec6d6cc5e18876b63782e38
Nicola Quaia <144346869+nicolaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-09-16 17:06:49 +0200

    Update assignment_01v2.py

--- assignment_01v2.py
+++ assignment_01v2.py
@@ -3,6 +3,9 @@ import math as m
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
+from scipy.optimize import minimize
+from scipy.special import gamma
+
 
 def plot_CDF(array_data, color, label):
     sorted_data_x = np.sort(array_data)
@@ -20,6 +23,42 @@ def plot_histogram(array_data, color, label):
     y = np.histogram(array_data, bins=100, density=True)
     width_bar = (max(array_data) - min(array_data)) / 100 + 0.1
     plt.bar(x, y[0], width=width_bar, align='center', color=color, label=label)
+
+def method1(mean,variance):
+    # Define the moments (mean and variance) of your data
+    #mean = 8.236
+    #variance = 15.272
+
+    # Define a function to calculate the moments of the Weibull distribution
+    def weibull_moments(params):
+        k, lambd = params
+        mu = lambd * gamma(1 + 1/k)
+        m2 = (lambd**2 * gamma(1 + 2/k)) - mu**2
+        return np.array([mu, m2])
+
+    #Define a function to calculate the difference between observed and estimated moments
+    def moment_error(params):
+        observed_moments = np.array([mean, variance])# + mean**2])
+        estimated_moments = weibull_moments(params)
+        return np.sum((observed_moments - estimated_moments)**2)
+
+    # Initial guesses for k and lambd
+    initial_guess = [1.0, 1.0]
+
+    # Minimize the error function to estimate parameters
+    result = minimize(moment_error, initial_guess, method='Nelder-Mead')
+
+    # Extract estimated parameters
+    estimated_k, estimated_lambda = result.x
+
+    # Print the estimated parameters
+    #print("Estimated k:", estimated_k)
+    #print("Estimated lambda:", estimated_lambda)
+    mu = estimated_lambda * gamma(1 + 1/estimated_k)
+    m2 = (estimated_lambda**2 * gamma(1 + 2/estimated_k)) - mu**2
+    return([estimated_k,estimated_lambda])
+
+
 
 
 # Hovsore data set
@@ -43,6 +82,7 @@ hov_std = np.std(hov[1])      #= sigma
 
 sprog_data = []
 
+# returns matrx with 2 columns, [0] is the 'cleaned' velocity and [1] is the direction
 with open("sprog.tsv") as file:
     tsv_file = csv.reader(file, delimiter="\t")
     for line in tsv_file:
@@ -52,13 +92,13 @@ with open("sprog.tsv") as file:
             elif float(line[3]) < 900:
                 sprog_data.append([float(line[1]), float(line[3])])
 
-#sprog_mean = np.mean(sprog_data, axis=0)
-#sprog_var = np.var(sprog_data, axis=0)
+sprog_mean = np.mean(sprog_data, axis=0)
+sprog_var = np.var(sprog_data, axis=0)
 
 #print(sprog_mean[0])                        # 8.224218093841214
 #print(sprog_var[0])                         # 15.241837914881087
 
-sprog_velocity = [row[0] for row in sprog_data]
+#sprog_velocity = [row[0] for row in sprog_data]
 #sprog_velocity_array = np.array(sprog_velocity)
 
 
@@ -76,4 +116,6 @@ sprog_velocity = [row[0] for row in sprog_data]
 plt.legend()
 plt.show()
 
-
+est_k,est_lambda = method1(sprog_mean[0],sprog_var[0])
+print(est_k)
+print(est_lambda)
